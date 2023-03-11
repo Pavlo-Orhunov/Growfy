8db6fdeb9acca0dd88230dcc1aeb4e7db14b1e98
Pavlo-Orhunov <pavlo.orhunov@gmail.com>
2023-03-11 18:59:27 +0200

    Some minor improvements in JS and CSS

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ const menuBody = document.querySelector(".menu__body")
 if (iconMenu) {
   // по клику на иконку меню
   iconMenu.addEventListener("click", function (e) {
-    // добавляем стиль _lock для блокирования прокрутки контента страницы при появлении меню
-    document.body.classList.toggle("_lock")
+    // добавляем стиль body--lock для блокирования прокрутки контента страницы при появлении меню
+    document.body.classList.toggle("body--lock")
     // переключаем (присваиваем или отбираем) класс _active для иконки бургера
     iconMenu.classList.toggle("_active")
     // переключаем (присваиваем или отбираем) класс _active для меню бургера
@@ -18,15 +18,25 @@ if (iconMenu) {
 }
 
 // -----Уменьшающийся при скролле header--------------
+// Get the header element
 const headerElement = document.querySelector(".header")
 
+// Define the callback function for the IntersectionObserver
 const callback = function (entries, observer) {
   if (entries[0].isIntersecting) {
-    headerElement.classList.remove("_scroll")
+    headerElement.classList.remove("header--scrolled")
   } else {
-    headerElement.classList.add("_scroll")
+    headerElement.classList.add("header--scrolled")
   }
 }
 
+// Create an IntersectionObserver instance with the callback function
 const headerObserver = new IntersectionObserver(callback)
-headerObserver.observe(headerElement)
+
+// Add error handling in case the header element is not found
+if (headerElement) {
+  // Observe the header element for changes
+  headerObserver.observe(headerElement)
+} else {
+  console.error("Header element not found.")
+}
